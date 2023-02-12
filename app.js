5c719383e53aee3c76d8675c44caf668d03ade2d
Iis Meysari <iismeysari230599@gmail.com>
2023-02-12 15:16:55 +0700

    initial commit

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const log = require('./middlewares/logger');
 app.use(log);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use("/public", express.static)
+app.use('/public', express.static);
 app.use(router);
 app.listen(3000, () => console.log('server:http://localhost:3000'));
--- routes.js
+++ routes.js
@@ -5,12 +5,91 @@ const upload = multer({ dest: 'uploads' });
 const fs = require('fs');
 const path = require('path');
 
+var contacts = [
+  {
+    id: '1',
+    name: 'meysari',
+  },
+];
+
+router.get('/contact', (req, res) => {
+  res.send({
+    success: true,
+    message: 'data fatched successfully',
+    data: contacts,
+  });
+});
+
 router.get('/', (req, res) => {
   res.send({
     status: 'succesfully',
     message: 'welcome to Express Js Tutorial',
   });
 });
+
+router.post('/contact', (req, res) => {
+  var name = req.body.name;
+  if (name) {
+    contacts.push({
+      id: (contacts.length + 1).toString(),
+      name: name,
+    });
+    res.send({
+      success: true,
+      message: 'data added successfully',
+    });
+  } else {
+    res.send({
+      success: false,
+      message: 'validation erorr',
+      erorrs: [
+        {
+          field: 'name',
+          message: 'cannot be null',
+        },
+      ],
+    });
+  }
+});
+
+router.delete('/contact/:id', (req, res) => {
+  var id = req.params.id;
+  var newContacts = contacts.filter((el) => el.id != id);
+  contacts = newContacts;
+
+  res.send({
+    success: true,
+    message: 'data deleted successfully',
+  });
+});
+
+router.put('/contact/:id', (req, res) => {
+  var id = req.params.id;
+  var name = req.body.name;
+  if (name) {
+    var index = contacts.findIndex((el) => el.id == id);
+    contacts[index] = {
+      ...contacts[index],
+      name: name,
+    };
+    res.send({
+      success: true,
+      message: 'data update successfully',
+    });
+  } else {
+    res.send({
+      success: false,
+      message: 'validation erorr',
+      erorrs: [
+        {
+          field: 'name',
+          message: 'cannot be null',
+        },
+      ],
+    });
+  }
+});
+
 router.get('/product/:id', (req, res) => {
   res.json({
     id: req.params.id,
