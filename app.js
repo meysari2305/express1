db453ef07afbf5a2b93f3d9fa484cbc81461fe52
Iis Meysari <iismeysari230599@gmail.com>
2023-03-06 19:15:44 +0700

    initial commit

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const path = require('path');
 const router = require('./routes');
 const log = require('./middlewares/logger');
+const cors = require('cors');
 
 app.use(log);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/public', express.static);
 app.use(router);
-app.listen(3000, () => console.log('server:http://localhost:3000'));
+app.listen(6000, () => console.log('server up and runing'));
